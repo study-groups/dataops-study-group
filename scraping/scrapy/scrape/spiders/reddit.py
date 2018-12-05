027a7adac2307ae15c566b607935518677fb1798
Ryan Harper <kimrharper@gmail.com>
2018-12-05 12:31:55 -0800

    fixed reddit scraper to scrape from old reddit

--- scraping/scrapy/scrape/spiders/reddit.py
+++ scraping/scrapy/scrape/spiders/reddit.py
@@ -6,13 +6,18 @@ class QuotesSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'https://www.reddit.com/r/LanguageTechnology/',
-            'https://www.reddit.com/r/LanguageTechnology/comments/a2obq0/out_of_vocabulary_word_embedding_in_a_pretrained/',
+            'https://old.reddit.com/search?q=nlp&restrict_sr=&sort=relevance&t=all',
+            'https://old.reddit.com/r/LanguageTechnology/comments/a1w7xk/efforts_to_crowdsource_linguistic_data/?st=jpbm595d&sh=e728b5b6',
         ]
+
+        # Headers to mimic a browser visit
+        headers = {'User-Agent': 'Mozilla/5.0'}
+
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, headers=headers, callback=self.parse)
 
     def parse(self, response):
+        print(response.body)
         page = response.url.split("/")[-2]
         filename = 'red-%s.html' % page
         with open(filename, 'wb') as f:
