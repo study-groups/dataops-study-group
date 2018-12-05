74a4cbf136ef09a913407bdbe9c558e15369bf1a
Ryan Harper <kimrharper@gmail.com>
2018-12-04 17:11:46 -0800

    added reddit spider

--- scraping/q-scraper/scrape/spiders/reddit.py
+++ scraping/q-scraper/scrape/spiders/reddit.py
@@ -2,19 +2,19 @@ import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
-    name = "quotes"
+    name = "reddit"
 
     def start_requests(self):
         urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
+            'https://www.reddit.com/r/LanguageTechnology/',
+            'https://www.reddit.com/r/LanguageTechnology/comments/a2obq0/out_of_vocabulary_word_embedding_in_a_pretrained/',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
+        filename = 'red-%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
