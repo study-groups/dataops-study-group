1a5d44958db943fb290083178a41193752809225
Ryan Harper <kimrharper@gmail.com>
2018-12-05 09:39:45 -0800

    added items field for reddit

--- scraping/q-scraper/scrape/items.py
+++ scraping/q-scraper/scrape/items.py
@@ -6,9 +6,16 @@
 # https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.item import Item, Field
 
 
 class ScrapeItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class RedditForum(Item):
+    """Livingsocial container (dictionary-like object) for scraped data"""
+    title = Field()
+    link = Field()
+    content = Field()
