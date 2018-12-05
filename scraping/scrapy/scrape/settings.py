3dcb78f7ddf959f9d6b0ee8ddcee7093e0df76df
Ryan Harper <kimrharper@gmail.com>
2018-12-05 12:30:55 -0800

    changes download throttle time

--- scraping/scrapy/scrape/settings.py
+++ scraping/scrapy/scrape/settings.py
@@ -27,7 +27,7 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 10
+DOWNLOAD_DELAY = 5
 
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
@@ -41,8 +41,8 @@ DOWNLOAD_DELAY = 10
 
 # Override the default request headers:
 #DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+  'Accept-Language': 'en',
 #}
 
 # Enable or disable spider middlewares
